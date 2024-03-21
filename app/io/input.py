3864528fe81b7bd902c6f0580b28add6b1bf2e13
myrrhv <miraaa31v@gmail.com>
2024-03-22 00:47:32 +0200

    Add functions

---
+++ app/io/input.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def input_from_console():
+    """
+    Функція для вводу тексту з консолі.
+    """
+    pass
+
+
+def read_from_file_builtin(file_path):
+    """
+    Функція для зчитування з файлу за допомогою вбудованих можливостей Python.
+
+    Параметри:
+        file_path (str): Шлях до файлу, який потрібно прочитати.
+    """
+    pass
+
+
+def read_from_file_with_pandas(file_path):
+    """
+    Функція для зчитування з файлу за допомогою бібліотеки pandas.
+
+    Параметри:
+        file_path (str): Шлях до файлу, який потрібно прочитати.
+    """
+    pass
+
